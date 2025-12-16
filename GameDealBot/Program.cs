5da641dd0ea3197741ebd33737a1eabd55e7e2c7
g-sadrack <Gabrielscr2023@gmail.com>
2025-12-16 20:00:24 -0300

    feat: initialize bot application

---
+++ GameDealBot/Program.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+
+    private static DiscordSocketClient? _client;
+
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+
+        var token = "SEU_TOKEN_AQUI";
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        
+        await _client.StartAsync();
+
+        await Task.Delay(Timeout.Infinite);
+    }
+}
