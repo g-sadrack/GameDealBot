6fa1bf9a3e09f16ed1e354396bc3056c2e983b22
g-sadrack <Gabrielscr2023@gmail.com>
2025-12-16 20:54:01 -0300

    feat: add a log event

--- GameDealBot/Program.cs
+++ GameDealBot/Program.cs
@@ -12,10 +12,18 @@ public class Program
 
         var token = "SEU_TOKEN_AQUI";
 
+        _client.Log += Log;
+
         await _client.LoginAsync(TokenType.Bot, token);
-        
+
         await _client.StartAsync();
 
         await Task.Delay(Timeout.Infinite);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
     }
 }
