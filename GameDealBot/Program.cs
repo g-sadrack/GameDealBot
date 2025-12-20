248554713ffa8521f8fa1633880cefe4d2b6cb34
g-sadrack <Gabrielscr2023@gmail.com>
2025-12-20 13:31:24 -0300

    add ready log

--- GameDealBot/Program.cs
+++ GameDealBot/Program.cs
@@ -15,6 +15,7 @@ public class Program
         var token = config["Discord:Token"];
 
         _client.Log += Log;
+        _client.Ready += Ready;
 
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
@@ -25,6 +26,11 @@ public class Program
     private static Task Log(LogMessage msg)
     {
         Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+    private static Task Ready()
+    {
+        Console.WriteLine($"Bot online como {_client?.CurrentUser.Username}!");
         return Task.CompletedTask;
     }
 }
